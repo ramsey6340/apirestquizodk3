584d8c89b309f90de785b56cfecbfd22f3bc72e2
Ibrahim Djingareye <maigaib82@gmail.com>
2023-07-27 15:02:35 +0000

    utilisateur first commit

--- src/main/java/com/groupe3/apirestquizodk3/controllers/UserController.java
+++ src/main/java/com/groupe3/apirestquizodk3/controllers/UserController.java
@@ -1,9 +1,33 @@
 package com.groupe3.apirestquizodk3.controllers;
 
+import com.groupe3.apirestquizodk3.models.User;
+import com.groupe3.apirestquizodk3.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/")
 public class UserController {
+    @Autowired
+    public UserService userService;
+    @GetMapping("users")
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+    @PostMapping("users")
+    public User addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
+    @GetMapping("users/{userId}")
+    public User getSpecificUser(@PathVariable Long userId){
+        return userService.getSpecificUser(userId);
+    }
+    @PostMapping("users/connect")
+    public  User getConnect(@RequestBody Map<String, String> connexionData){
+        return userService.getConnect(connexionData);
+    }
 
 }
--- src/main/java/com/groupe3/apirestquizodk3/services/UserService.java
+++ src/main/java/com/groupe3/apirestquizodk3/services/UserService.java
@@ -1,7 +1,32 @@
 package com.groupe3.apirestquizodk3.services;
+import com.groupe3.apirestquizodk3.models.User;
+import com.groupe3.apirestquizodk3.repositories.UserRepository;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
 @Service
 @Data
 public class UserService {
+    @Autowired
+    public UserRepository userRepository;
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+    public  User addUser(User user){
+        return userRepository.save(user);
+    }
+     public User  getSpecificUser(Long userId){
+        return userRepository.findByUserId(userId);
+     }
+     public  User getConnect (Map<String, String> connexionData){
+        String login = connexionData.get("login");
+        String password = connexionData.get("password");
+        return userRepository.findByLoginAndPassword(login, password);
+
+     }
+
 }
