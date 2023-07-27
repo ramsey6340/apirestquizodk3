ac30a46d01925cf8c95201ff6edf6ae9739ea17d
Ibrahim Djingareye <maigaib82@gmail.com>
2023-07-27 15:02:35 +0000

    utilisateur first commit

--- src/main/java/com/groupe3/apirestquizodk3/repositories/UserRepository.java
+++ src/main/java/com/groupe3/apirestquizodk3/repositories/UserRepository.java
@@ -4,6 +4,13 @@ import com.groupe3.apirestquizodk3.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(Long userId);
+    List<User> findAll();
+    User findByLoginAndPassword(String login, String password);
+
+
 }
